affd4d0054dc762daf817831a28f22025b87e3b7
benghui <benghui@gmail.com>
2022-08-21 14:24:31 +0800

    Setup  query routing

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-
 import express from "express";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
+app.get('/drinks', async (req, res) => {
+	if (req.query.type === 'coffee') {
+		res.send("coffee")
+	} else if (req.query.type === 'beer') {
+		res.send("beer");
+	} else if (Object.keys(req.query).length === 0) {
+		res.send('coffee beer');
+	} else {
+		res.send("err")
+	}
 })
 
 app.listen(port, () => console.log(`AHOY listening on PORT ${port}`));
