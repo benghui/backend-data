d29ea15fc9e954f9d3cc8945b5681466ad6666e6
benghui <benghui@gmail.com>
2022-08-22 22:56:22 +0800

    Add async error handling middleware

--- index.js
+++ index.js
@@ -1,34 +1,23 @@
 import express from "express";
 import { transformAllCoffeeData, transformAllBeerData, transformAllData } from "./etl.js";
+import { catchAsyncErrors } from "./catchAsyncMiddleware.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/drinks', async (req, res) => {
+app.get('/drinks', catchAsyncErrors(async (req, res, next) => {
 	if (req.query.type === 'coffee') {
-		try {
-			const data = await transformAllCoffeeData();
-			res.send(data);
-		} catch (err) {
-			res.status(400).send(err);
-		}
+		const data = await transformAllCoffeeData();
+		res.send(data);
 	} else if (req.query.type === 'beer') {
-		try {
-			const data = await transformAllBeerData();
-			res.send(data);
-		} catch (err) {
-			res.status(400).send(err);
-		}
+		const data = await transformAllBeerData();
+		res.send(data);
 	} else if (Object.keys(req.query).length === 0) {
-		try {
-			const data = await transformAllData();
-			res.send(data);
-		} catch (err) {
-			res.status(400).send(err);
-		}
+		const data = await transformAllData();
+		res.send(data);
 	} else {
-		res.send("err")
+		res.status(400).send("Server encountered an error with query type")
 	}
-})
+}))
 
 app.listen(port, () => console.log(`AHOY listening on PORT ${port}`));
